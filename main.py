28800382dd5e1bbcd2f73b6e414762094f2eac88
Matthias Carré <lemattheuxbis@gmail.com>
2024-07-12 19:07:10 +0200

    Ecriture de texte simple avec maj

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+from PIL import Image, ImageDraw, ImageFilter
+
+pos=1
+
 def imagelenght(string):
     res=0
     for letter in string:
         match letter:
             case " ":
-                res+=1
+                res+=2
             case "I":
                 res+=3
             case "i":
@@ -20,7 +24,57 @@ def imagelenght(string):
                 res+=5
             case _:
                 res+=4
+        res+=1
     return res
 
-n=imagelenght("v ")
-print(n)
+def letterPosition(letter): #return (pos,lenght,isMaj)
+    Lmin=[1,6,11,16,21,26,31,36,41,43,47,52,55,61,66,71,76,81,86,91,96,101,107,113,118,123]
+    Lmax=[1,6,11,16,21,26,31,36,41,45,50,55,60,66,71,76,81,86,91,96,102,107,113,119,124,129]
+
+    if(letter == " "):
+        return (135,1,True)
+    val=ord(letter)-64
+
+    if(val<26):
+        return(Lmax[val-1],Lmax[val]-Lmax[val-1]-1,True)
+    else:
+        val=ord(letter)-96
+        return (Lmin[val-1],Lmin[val]-Lmin[val-1]-1,False)
+    
+
+def addLetter(letter,image):
+    global pos
+    
+    letterPos,letterLenght,isMaj = letterPosition(letter)
+    if(isMaj):
+        alphabet = Image.open("alphabetMaj.png")
+    else:
+        alphabet = Image.open("alphabetMin.png")
+    mask_letter = Image.new("L",alphabet.size,0)
+    draw = ImageDraw.Draw(mask_letter)
+    draw.rectangle((letterPos,1,letterPos+letterLenght,9),fill=255)
+    image.paste(alphabet,(pos-letterPos,0),mask_letter)
+    pos+=letterLenght+1
+    #print(f"valeur de pos et taille:{letterPos}, {letterLenght}")
+    
+    
+
+def writemsg(msg):
+    test=Image.new("RGBA",(imagelenght(msg),10),color=255)
+    print(imagelenght(msg))
+    for e in msg:
+        addLetter(e,test)
+    test.save("res.png")
+
+
+print(ord('A'))
+writemsg("ABRUITIT DE MATTHIAS")
+#alphabet = Image.open("alphabetMin.png")
+#mask_im = Image.new("L",alphabet.size,0)
+#draw = ImageDraw.Draw(mask_im)
+#draw.rectangle((6,1,9,9),fill=255)
+#test.paste(alphabet,(5,0),mask_im)
+#test.paste(alphabet,(-5,0),mask_im)
+#mask_im.save("mask.png")
+
+
